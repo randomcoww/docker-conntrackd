3cab46de025de2088c12cdbd4332ad9dc9aba653
randomcoww <etherblimp@gmail.com>
2019-03-16 19:35:26 -0700

    Fix config file name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "$CONNTRACKD_LOCAL_CONFIG" ]; then
-  echo -en "$CONNTRACKD_LOCAL_CONFIG" > /etc/conntrackd/conntrackd.con
+  echo -en "$CONNTRACKD_LOCAL_CONFIG" > /etc/conntrackd/conntrackd.conf
 fi
 
 exec /usr/sbin/conntrackd \
